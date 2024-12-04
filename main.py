0e6b6289108a2d392eeda8c25779635c002f8bea
BAHEN <olegcuzanskij@gmail.com>
2024-12-04 23:28:00 +0200

    com

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 def calculate_bmi(weight, height):
     return weight/(height ** 2)
 
-def bmi_calculator():
-    weight = float(input("Enter your weight in kilograms: "))
-    height = float(input("Enter your height in meters: "))
-    bmi = calculate_bmi(weight, height)
-    print(f"Your BMI is: {bmi:.2f}")
+def bmi_calculator(bmi):
+    
 
     if bmi < 18.5:
         print("You are underweight.")
@@ -16,4 +13,14 @@ def bmi_calculator():
     else:
         print("You are obese.")
 
-bmi_calculator()
+
+def main():
+    weight = float(input("Enter your weight in kilograms: "))
+    height = float(input("Enter your height in meters: "))
+    bmi = calculate_bmi(weight, height)
+    print(f"Your BMI is: {bmi:.2f}")
+    bmi_calculator(bmi)
+    input()
+
+if __name__ == "__main__":
+    main()
